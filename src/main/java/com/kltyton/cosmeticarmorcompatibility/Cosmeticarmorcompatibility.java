b637deb8a4e4080313d691ede8875809d6462a65
kltyton <h3193778245@163.com>
2025-10-10 18:20:55 +0800

    init

---
+++ src/main/java/com/kltyton/cosmeticarmorcompatibility/Cosmeticarmorcompatibility.java
@@ -0,0 +1,10 @@
+package com.kltyton.cosmeticarmorcompatibility;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cosmeticarmorcompatibility implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/kltyton/cosmeticarmorcompatibility/client/CosmeticarmorcompatibilityClient.java
@@ -0,0 +1,10 @@
+package com.kltyton.cosmeticarmorcompatibility.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CosmeticarmorcompatibilityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
