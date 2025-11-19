e04e0a918fc542425e15859b0e6d21514dd1a7bf
kltyton <h3193778245@163.com>
2025-11-19 16:07:54 +0800

    1.1.0

--- src/client/java/com/kltyton/cosmeticarmorcompatibility/mixin/client/ModelPartMixin.java
+++ src/client/java/com/kltyton/cosmeticarmorcompatibility/mixin/client/ModelPartMixin.java
@@ -1,11 +1,14 @@
-package com.kltyton.cosmeticarmorcompatibility.mixin;
+package com.kltyton.cosmeticarmorcompatibility.mixin.client;
 
 import io.github.apace100.cosmetic_armor.CosmeticArmor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,10 +16,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(value = Player.class, priority = 0)
-public class ModelPartMixin {
+public abstract class ModelPartMixin extends LivingEntity {
+    protected ModelPartMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @Inject(method = "getItemBySlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/EquipmentSlot;getType()Lnet/minecraft/world/entity/EquipmentSlot$Type;"), cancellable = true)
     public void getItemBySlot(EquipmentSlot equipmentSlot, CallbackInfoReturnable<ItemStack> cir) {
-        if (equipmentSlot.getType() == EquipmentSlot.Type.ARMOR) {
+        if (this.level().isClientSide() && equipmentSlot.getType() == EquipmentSlot.Type.ARMOR) {
             ItemStack cosmeticStack = CosmeticArmor.getCosmeticArmor((Player) (Object) this, equipmentSlot);
             if (!cosmeticStack.isEmpty()) {
                 cir.setReturnValue(cosmeticStack);
